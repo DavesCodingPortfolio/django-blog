fe86fe25d4a0a7ff6b1955c1852e6de2e8870f2a
DavesCodingPortfolio <davestablet091@gmail.com>
2026-02-24 19:59:47 +0000

    might work this time - settings.py

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-yek2j+967qdprs)#clg+^_l#@voh9g_n=i5_0q2g$eh)k6=&#3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [,'.herokuapp.com']
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
